bd08c8085197b1ed2e59076d09e7347cc301bed0
Arafath76 <ali2frnds@gmail.com>
2020-10-17 19:25:13 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
+var particles=[];
+var plinkos=[];
+var division=[];
+var divisionHeight=300;
+
+
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
+  for (var k=0;k<=innerWidth;k=k+80){
+    divisions.push(new Divisions(k,height-divisionHeight/2,2,10,divisionHeight));
+  }
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+
   drawSprites();
 }
